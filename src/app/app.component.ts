a150c761b59ce76aebfca94c6428052eeb8a7f39
shahed <shahed.rahman88@gmail.com>
2022-03-06 20:53:23 +0600

    recipe-item populated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'recipe';
+  // title = 'recipe';
+  loadedFeature = "recipe"
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
