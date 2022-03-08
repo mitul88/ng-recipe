1fce7d8ec631e814c6746befbfe4c59d2e1e9d56
shahed <shahed.rahman88@gmail.com>
2022-03-08 19:23:29 +0600

    ingredients display

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.mode";
 import { Recipe } from "./recipe.model";
 
 
@@ -6,8 +7,25 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Samusa', 'A great evening snack', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
-        new Recipe('Singara', 'Great alternative for Samusa', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
+        new Recipe(
+                'Samusa', 
+                'A great evening snack', 
+                'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg', 
+                [
+                    new Ingredient('flour', 1),
+                    new Ingredient('meat', 1),
+                ]
+            ),
+        new Recipe(
+                'Singara', 
+                'Great alternative for Samusa', 
+                'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg', 
+                [
+                    new Ingredient('flour', 1),
+                    new Ingredient('potato', 1),
+                    new Ingredient('beans', 1),
+                ]
+            ),
       ]
 
       getRecipes() {
