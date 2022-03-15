1736f125931a6783ef7810b07aec5dfffed9dbc1
shahed <shahed.rahman88@gmail.com>
2022-03-15 20:09:07 +0600

    update button and functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -12,7 +12,7 @@ import { ShoppingListService } from '../shopping-list.service';
 export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   @ViewChild('f') slForm: NgForm;
-  
+
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
@@ -42,6 +42,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if(this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient)
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
   }
 }
