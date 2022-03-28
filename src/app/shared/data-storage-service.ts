d8e66b8f21972e700c1bfbc42e40b4493798df1b
shahed <shahed.rahman88@gmail.com>
2022-03-28 20:09:23 +0600

    recipeService injected in data-storage

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: "root"})
 export class DataStorageService {
-    constructor( private http: HttpClient ){
-        
+    constructor( private http: HttpClient, private recipeService: RecipeService ){
+
+    }
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
     }
 }
