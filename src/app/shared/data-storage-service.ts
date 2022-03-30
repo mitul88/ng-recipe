d3cabe39b8438647ab0c49029bcf665ed34c2b9e
shahed <shahed.rahman88@gmail.com>
2022-03-30 14:17:57 +0600

    ingredients property fetch bug fix

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: "root"})
 export class DataStorageService {
@@ -20,6 +21,11 @@ export class DataStorageService {
     fetchRecipes() {
         this.http
             .get<Recipe[]>('https://ng-recipebook-6dd2c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
+            .pipe(map(recipes=>{
+                return recipes.map(recipe=> {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                })
+            }))
             .subscribe(recipes=> {
                 this.recipeService.setRecipes(recipes);
             })       
