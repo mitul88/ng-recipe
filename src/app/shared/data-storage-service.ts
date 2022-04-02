c218eb0b42ffa6f1a796c99dca02865fe98619a3
shahed <shahed.rahman88@gmail.com>
2022-04-02 12:25:59 +0600

    route resolver added to detail and edit component

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
+
 
 @Injectable({providedIn: "root"})
 export class DataStorageService {
@@ -19,15 +20,16 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
             .get<Recipe[]>('https://ng-recipebook-6dd2c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
             .pipe(map(recipes=>{
                 return recipes.map(recipe=> {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
                 })
-            }))
-            .subscribe(recipes=> {
+            }),
+            tap(recipes=> {
                 this.recipeService.setRecipes(recipes);
-            })       
+            })   
+        )
     }
 }
