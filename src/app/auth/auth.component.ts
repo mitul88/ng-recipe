abbdbc5d75463163b64e2daf282d17c4a7ba24df
shahed <shahed.rahman88@gmail.com>
2022-04-02 22:05:09 +0600

    auth service created

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,8 +11,9 @@ export class AuthComponent {
 
   constructor() { }
 
-  onSubmit(form) {
-
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
   }
 
   onSwitchMode() {
