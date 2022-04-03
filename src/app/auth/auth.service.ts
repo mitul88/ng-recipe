95ba255baa5ddd98c981c02363260500a3452042
shahed <shahed.rahman88@gmail.com>
2022-04-03 15:28:03 +0600

    recieving err msg from backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 interface AuthResponseData {
   idToken: string;
@@ -23,6 +26,21 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
+    ).pipe(catchError(errorRes => {
+          let errorMessage = "An unknown error occured !";
+
+          if(!errorRes.error || !errorRes.error.error) {
+            return throwError(()=> new Error(errorMessage))
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = "This email already exists"
+          }
+          
+          return throwError(()=> new Error(errorMessage));
+        }
+      )
     );
   }
 
