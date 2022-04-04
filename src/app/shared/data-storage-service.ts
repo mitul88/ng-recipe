97fe34b0bf51aa881e2fff61402718c6219f22d7
shahed <shahed.rahman88@gmail.com>
2022-04-04 14:57:37 +0600

    used exhaustMap to initate http and replace user obs

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -1,13 +1,17 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { map, tap } from "rxjs/operators";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 
 @Injectable({providedIn: "root"})
 export class DataStorageService {
-    constructor( private http: HttpClient, private recipeService: RecipeService ){
+    constructor( 
+        private http: HttpClient, 
+        private recipeService: RecipeService,
+        private authService: AuthService ){
 
     }
 
@@ -20,16 +24,23 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-            .get<Recipe[]>('https://ng-recipebook-6dd2c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
-            .pipe(map(recipes=>{
+        return this.authService.user.pipe(
+                take(1), exhaustMap(user => {
+                return this.http
+                .get<Recipe[]>(
+                    'https://ng-recipebook-6dd2c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',
+                        {
+                            params: new HttpParams().set('auth', user.token)
+                        }
+                    )
+            }), map(recipes=>{
                 return recipes.map(recipe=> {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
                 })
             }),
             tap(recipes=> {
                 this.recipeService.setRecipes(recipes);
-            })   
+            } )
         )
     }
 }
