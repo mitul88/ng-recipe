52ffb6677d778c88f381c6420a754ede0a83101f
shahed <shahed.rahman88@gmail.com>
2022-04-06 21:04:17 +0600

    AuthGuard added to recipe & fixed bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,24 +8,30 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuardService } from './auth/auth.guard';
 
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
-    {path: '', component: RecipeStartComponent },
-    {path: 'new', component: RecipeEditComponent },
-    {
-      path: ':id', 
-      component: RecipeDetailComponent, 
-      resolve: [RecipesResolverService]
-    },
-    {
-      path: ':id/edit', 
-      component: RecipeEditComponent,
-      resolve: [RecipesResolverService]
-    },
-  ]},
+  {
+    path: 'recipes', 
+    component: RecipesComponent,
+    canActivate: [AuthGuardService], 
+    children: [
+      {path: '', component: RecipeStartComponent },
+      {path: 'new', component: RecipeEditComponent },
+      {
+        path: ':id', 
+        component: RecipeDetailComponent, 
+        resolve: [RecipesResolverService]
+      },
+      {
+        path: ':id/edit', 
+        component: RecipeEditComponent,
+        resolve: [RecipesResolverService]
+      },
+    ]
+  },
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: 'auth', component: AuthComponent},
 ];
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -4,12 +4,12 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
-Injectable({providedIn: "root"})
-export class AuthGuard implements CanActivate {
+@Injectable({ providedIn: 'root'})
+export class AuthGuardService implements CanActivate {
 
     constructor(private authService: AuthService) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): boolean | Promise<boolean> | Observable<boolean>  {
        return this.authService.user.pipe(map(user => {
            return !!user;
        }));
