f8908554199c2ba79abbcbe3a0eeadad2364693c
shahed <shahed.rahman88@gmail.com>
2022-04-06 15:03:46 +0600

    created auto login and initiated it in app component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -54,6 +54,28 @@ export class AuthService {
     }));;    
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if(!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email, 
+      userData.id, 
+      userData._token, 
+      new Date(userData._tokenExpirationDate)
+    );
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
