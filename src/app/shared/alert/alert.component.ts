e590abd110ea975ccab87e163b7e6e3cd315834e
shahed <shahed.rahman88@gmail.com>
2022-04-07 11:36:30 +0600

    alert css and message property added

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-alert',
-    templateUrl: './alert.component.html'
+    templateUrl: './alert.component.html',
+    styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent {
+    @Input() message: string;
 
 }
