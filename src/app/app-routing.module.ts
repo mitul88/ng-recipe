b87890b403bf8b4a0d1a0bf69a0596eb674d664e
shahed <shahed.rahman88@gmail.com>
2022-04-09 16:09:44 +0600

    auth module created and initiated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { AuthComponent } from './auth/auth.component';
 
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'auth', component: AuthComponent},
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
 
 @NgModule({
