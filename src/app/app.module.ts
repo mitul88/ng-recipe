3f5335a61ab3360be508a1a9953f82631fc15a47
shahed <shahed.rahman88@gmail.com>
2022-04-09 15:36:51 +0600

    moved all common modules to shared declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,20 +12,16 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list-module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +30,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list-module';
     ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
--- src/app/shopping-list/shopping-list-module.ts
+++ src/app/shopping-list/shopping-list-module.ts
@@ -1,7 +1,9 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+
+import { SharedModule } from "../shared/shared.module";
+
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
 
@@ -13,10 +15,10 @@ import { ShoppingListComponent } from "./shopping-list.component";
     ],
     imports: [
         FormsModule,
-        CommonModule,
         RouterModule.forChild([
             {path: 'shopping-list', component: ShoppingListComponent}
-        ])
+        ]),
+        SharedModule
     ]
 })
 export class ShoppingListModule {
